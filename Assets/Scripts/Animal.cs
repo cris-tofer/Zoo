4a97c3d48d3054cafa7f8a9a37c0e7255cdc61ce
cris-tofer <chrisnf05@gmail.com>
2025-02-17 14:18:45 -0500

    added perameters for the animals

--- Assets/Scripts/Animal.cs
+++ Assets/Scripts/Animal.cs
@@ -6,18 +6,35 @@ public abstract class Animal
 {
     public abstract void move(Vector2 direction);
     public float moveSpeed;
-    public float health;
+    public float maxHealth;
+    public float curHealth;
 }
 public class Seal : Animal
 {
     public override void move(Vector2 direction)
     {
+        moveSpeed = 1;
+    }
 
+    public float fat;
+    public void swim()
+    {
+        moveSpeed = curHealth;
     }
 }
 public class Monkey : Animal
 {
     public override void move(Vector2 direction)
+    {
+
+    }
+
+    public float volume;
+    public void sling()
+    {
+
+    }
+    public void scream()
     {
 
     }
@@ -28,10 +45,22 @@ public class Lion : Animal
     {
 
     }
+
+    public float volume;
+    public void roar()
+    {
+
+    }
 }
 public class Giraffe : Animal
 {
     public override void move(Vector2 direction)
+    {
+
+    }
+
+    public float neckLength;
+    public void swingNeck()
     {
 
     }
---
+++ Assets/Scripts/Animals.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animals : MonoBehaviour
+{
+    Animal Lion = new Lion();
+    Animal Seal = new Seal();
+    Animal Giraffe = new Giraffe();
+    Animal Monkey= new Monkey();
+    // Start is called before the first frame update
+    void Start()
+    {
+        Lion.maxHealth = 8;
+        Lion.curHealth = Lion.maxHealth;
+        Lion.moveSpeed = 8;
+
+        Seal.maxHealth = 10;
+        Seal.curHealth = Seal.maxHealth;
+        Seal.moveSpeed = 1;
+
+        Giraffe.maxHealth = 20;
+        Giraffe.curHealth = Giraffe.maxHealth;
+        Giraffe.moveSpeed = 20;
+
+        Monkey.maxHealth = 5;
+        Monkey.curHealth = Monkey.maxHealth;
+        Monkey.moveSpeed = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
