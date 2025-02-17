a14fdfac09121e3da343b596da637a234cd56876
cris-tofer <chrisnf05@gmail.com>
2025-02-17 13:25:57 -0500

    changed the script structure

--- Assets/Scripts/Animal.cs
+++ Assets/Scripts/Animal.cs
@@ -2,17 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Animal
+public abstract class Animal
 {
-    // Start is called before the first frame update
-    void Start()
+    public abstract void move(Vector2 direction);
+    public float moveSpeed;
+    public float health;
+}
+public class Seal : Animal
+{
+    public override void move(Vector2 direction)
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    }
+}
+public class Monkey : Animal
+{
+    public override void move(Vector2 direction)
     {
-        
+
+    }
+}
+public class Lion : Animal
+{
+    public override void move(Vector2 direction)
+    {
+
+    }
+}
+public class Giraffe : Animal
+{
+    public override void move(Vector2 direction)
+    {
+
     }
 }
